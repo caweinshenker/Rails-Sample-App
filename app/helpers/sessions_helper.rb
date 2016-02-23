9e1b335c1fb77762d43cccdaa4ab320b9d08d7a6
Colin Weinshenker <caweinshenker@email.wm.edu>
2016-02-23 00:25:13 -0500

    Finish user edit, update, index, still testing errors

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -40,5 +40,15 @@ module SessionsHelper
     @current_user= nil
   end
 
+  #Redirec to stored location (or default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
 
 end
